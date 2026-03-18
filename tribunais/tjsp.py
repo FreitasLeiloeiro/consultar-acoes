2b57d61bba2f5e146cd06d88ebfc088e8df98c50
FreitasLeiloeiro <consultaacao@gmail.com>
2026-03-18 11:07:13 -0300

    Update tjsp.py

--- tribunais/tjsp.py
+++ tribunais/tjsp.py
@@ -1,1 +1,10 @@
-
+def buscar_tjsp(nome):
+    return [
+        {
+            "Tribunal": "TJSP",
+            "Processo": "Conexão OK",
+            "Classe": "Teste de integração",
+            "Data": "01/01/2024",
+            "Risco": "⚠"
+        }
+    ]
